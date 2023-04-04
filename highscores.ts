9a97b72e00843390657382c7c5b36fc383a5fe0a
blksy <79380937+blksy@users.noreply.github.com>
2023-04-04 19:17:08 +0200

    [ts-migrate][Quiz_app] Run TS Migrate
    Co-authored-by: ts-migrate <>

--- highscores.ts
+++ highscores.ts
@@ -1,8 +1,10 @@
 const highScoresList = document.getElementById("highScoresList");
+// @ts-expect-error TS(2451): Cannot redeclare block-scoped variable 'highScores... Remove this comment to see the full error message
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+// @ts-expect-error TS(2531): Object is possibly 'null'.
 highScoresList.innerHTML = highScores
-  .map(score => {
+  .map((score: any) => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
   })
   .join("");
